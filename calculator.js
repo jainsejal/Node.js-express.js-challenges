353ba4b091bb288a817e352e9e251d3aab7ac009
jainsejal <sejaljain1194@gmail.com>
2020-07-01 10:02:04 +0200

    BMI and basic calculator

--- calculator.js
+++ calculator.js
@@ -15,6 +15,17 @@ app.post("/",function(req, res) {
   res.send("The result of your calculation is "+ result);
 });
 
+app.get("/bmicalculator", function(req,res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator",function(req, res) {
+  var wt = parseFloat(req.body.wei);
+  var ht = parseFloat(req.body.hei);
+  var result = (wt/(ht*ht));
+  res.send("Your BMI is " + result);
+});
+
 app.listen(3000,function() {
   console.log("Server is running");
 });
